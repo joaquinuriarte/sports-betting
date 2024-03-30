b81c754ac086888d54b7e48e38f731fdb00d515f
Joaquin Uriarte <juriartebruno@gmail.com>
2024-03-29 22:15:50 -0700

    Add DataIO Abstract Base Class
    Created the abstract base class for the DataIO module.
    
    Instead of taking path and list of columns as input, it only takes a dictionary with the model's configuration. The dictionary contains all of the sources, columns of each source, and how to properly join each source to create a final df.
    
    The read_df_from_path method should worry about reading and joining data.

--- modules/input_output/data_source.py
+++ modules/input_output/data_source.py
@@ -1,6 +1,18 @@
 from abc import ABC, abstractmethod
+from typing import Dict, Any
+import pandas as pd
 
 class DataIO(ABC):
+
     @abstractmethod
-    def read_from_path(self, path: str, columns: list[str]):
+    def read_df_from_path(self, model_config: Dict[str, Any]) -> pd.DataFrame:
+        """
+        Reads and optionally joins data based on the model configuration.
+
+        Parameters:
+        - model_config (Dict[str, Any]): Configuration dictionary including paths, columns, and optional joins.
+
+        Returns:
+        - pd.DataFrame: The resulting dataframe after loading and optional joining operations.
+        """
         pass
