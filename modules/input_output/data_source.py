756b67d81710d852fa7874c8ac021b1baaabbf16
Joaquin Uriarte <juriartebruno@gmail.com>
2024-04-02 20:30:23 -0700

    Modified DataIO Base Class Inputs
    Changed inputs back to path & list of columns

--- modules/input_output/data_source.py
+++ modules/input_output/data_source.py
@@ -1,16 +1,17 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Any
+from typing import List
 import pandas as pd
 
 class DataIO(ABC):
 
     @abstractmethod
-    def read_df_from_path(self, model_config: Dict[str, Any]) -> pd.DataFrame:
+    def read_df_from_path(self, path: str, columns: List[str]) -> pd.DataFrame:
         """
         Reads and optionally joins data based on the model configuration.
 
         Parameters:
-        - model_config (Dict[str, Any]): Configuration dictionary including paths, columns, and optional joins.
+        - path (str): Path to data source
+        - columns (List[str]): Target columns from data source 
 
         Returns:
         - pd.DataFrame: The resulting dataframe after loading and optional joining operations.
