e992e53ff199db90bf458c1002f699fbe725af01
Joaquin Uriarte <juriartebruno@gmail.com>
2024-08-26 09:43:09 -0700

    Refactored DatasetGenerator Class
    DatasetGenerator class got broken into two classes each in separate files. We have dataset_loader to take care of loading model configuration and reading data sources and dataset_generator to generate the resulting dataframe from the reads performed by dataset_loader. I also decoupled some functions to be independent of class state and take in arguments instead of referencing class variables.

--- modules/processor/dataset_generator/dataset_generator.py
+++ modules/processor/dataset_generator/dataset_generator.py
@@ -1,91 +1,36 @@
 import pandas as pd
-from modules.input_output.handlers.csv_io import CsvIO
-from modules.input_output.handlers.txt_io import TxtIO
-from modules.input_output.handlers.xml_io import XmlIO
-from utils.wrappers.source import Source
-from utils.wrappers.dataset_config import DatasetConfig
-from configuration.config_manager import load_model_config
 
 
 class DatasetGenerator:
     """
-    Class responsible for generating datasets based on a configuration file and model name.
-    This class orchestrates the loading of data configurations, reading data from specified sources,
-    and optionally setting primary keys for each dataframe.
+    Handles the dataset generation by performing a join operation on the provided DataFrames.
+    This class is responsible for:
+    - Accepting the left and right DataFrames, along with join configuration.
+    - Performing the join operation based on the provided join type and keys.
+    - Returning the resulting merged DataFrame.
     """
-
-    def __init__(self, config_path: str, model_name: str):
+    def __init__(self, dataframe_left: pd.DataFrame, dataframe_right: pd.DataFrame, join_type: str, join_key: list):
         """
-        Initialize the DatasetGenerator with the configuration path and model name.
-
-        :param config_path: Path to the configuration file.
-        :param model_name: Name of the model for which to generate the dataset.
+        Initialize the DatasetGenerator with the dataframes and join settings.
+        :param dataframe_left: The left DataFrame to be joined.
+        :param dataframe_right: The right DataFrame to be joined.
+        :param join_type: The type of join to perform (e.g., 'inner', 'outer').
+        :param join_key: The key(s) to join on.
         """
-        self.config_path = config_path
-        self.model_name = model_name
-        self.dataset_config = None
-        self.dataframe_left = None
-        self.dataframe_right = None
-        self.join_type = None
-        self.join_key = None
+        self.dataframe_left = dataframe_left
+        self.dataframe_right = dataframe_right
+        self.join_type = join_type
+        self.join_key = join_key
 
-        # Load the configuration and then read data sources upon initialization
-        self.load_config()
-        self.read_data_sources()
-        self.process_dataframes()
-
-    def load_config(self):
+    def generate_dataset(self):
         """
-        Load the model configuration and instantiate Source and DatasetConfig objects.
-        Reads configuration from a YAML file and creates source instances for each data source specified.
+        Perform the join operation and return the resulting DataFrame.
         """
-        model_config = load_model_config(self.config_path, self.model_name)
-        sources = []
-        for source_info in model_config["sources"]:
-            file_type = source_info["file_type"]
-            if file_type == "csv":
-                file_reader = CsvIO()
-            elif file_type == "xml":
-                file_reader = XmlIO()
-            elif file_type == "txt":
-                file_reader = TxtIO()
-
-            # Create a Source instance with the appropriate file reader
-            source_instance = Source(
-                path=source_info["path"],
-                columns=source_info["columns"],
-                join_side=source_info["join_side"],
-                file_reader=file_reader,
-            )
-            sources.append(source_instance)
-
-        self.join_type = model_config["join_type"]
-        self.join_key = model_config["join_keys"]
-        self.dataset_config = DatasetConfig(sources=sources)
-
-    def read_data_sources(self):
-        """
-        Read data from each configured source path and store the resulting DataFrames.
-        DataFrames are indexed by their source paths for easy access.
-        If a primary key is specified for a source, it is set as the index of the DataFrame.
-        """
-        for source in self.dataset_config.sources:
-            df = source.file_reader.read_df_from_path(source.path, source.columns)
-            if source.join_side == "right":
-                self.dataframe_right = df
-            else:
-                self.dataframe_left = df
-
-    def process_dataframes(self):
-        """
-        Merge the left and right DataFrames using the specified join type.
-        """
-        merged_df = pd.merge(
-            self.dataframe_left,
+        # Perform the join operation
+        joined_df = self.dataframe_left.merge(
             self.dataframe_right,
-            on=self.join_key,  # Change from index join to column join
             how=self.join_type,
+            on=self.join_key
         )
-        return merged_df
-        # choose feature processor? & run it with merged_df
-        # return final, usable df
+        return joined_df
+
