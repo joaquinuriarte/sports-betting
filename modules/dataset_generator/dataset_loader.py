ed9ff7db9e9f507d981b8abe64ef95fa9306cf13
Joaquin Uriarte <juriartebruno@gmail.com>
2024-10-20 19:55:04 -0700

    Modified dataset_loader to use datastructures

--- modules/dataset_generator/dataset_loader.py
+++ modules/dataset_generator/dataset_loader.py
@@ -1,17 +1,26 @@
 from typing import List
 import pandas as pd
 from modules.dataset_generator.interfaces.data_io_interface import DataIO
+from modules.data_structures.dataset_config import Source
+
 
 class DatasetLoader:
     """
     Handles loading the data sources as DataFrames.
     """
 
-    def __init__(self, data_loaders: List[DataIO], data_loaders_sources: List[dict]):
-        self.data_loaders = data_loaders
-        self.data_loaders_sources = data_loaders_sources
+    def __init__(self, data_loaders: List[DataIO], sources: List[Source]):
+        """
+        Initializes the DatasetLoader with data readers and sources.
 
-    def load_data(self) -> List[pd.DataFrame]: # TODO: Should we wrap this List of DF or is it okay to keep it like this?
+        Args:
+            data_loaders (List[DataIO]): List of DataIO readers for each source.
+            sources (List[Source]): List of Source objects containing paths and columns.
+        """
+        self.data_loaders = data_loaders
+        self.sources = sources
+
+    def load_data(self) -> List[pd.DataFrame]:
         """
         Loads the data from all sources.
         
@@ -19,6 +28,7 @@ class DatasetLoader:
             list: List of DataFrames for each data source.
         """
         dataframes = []
-        for loader, source in zip(self.data_loaders, self.data_loaders_sources):
-            dataframes.append(loader.read_df_from_path(path=source['path'], columns=source['columns']))
+        for loader, source in zip(self.data_loaders, self.sources):
+            df = loader.read_df_from_path(path=source.path, columns=source.columns)
+            dataframes.append(df)
         return dataframes
