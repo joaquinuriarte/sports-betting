ee91188f6220de8ffaa4252f6761bf1ffc8a3894
Joaquin Uriarte <juriartebruno@gmail.com>
2024-10-20 19:40:34 -0700

    Update dataset_generator to use datastructures

--- modules/dataset_generator/dataset_generator.py
+++ modules/dataset_generator/dataset_generator.py
@@ -1,11 +1,12 @@
-import pandas as pd
-from typing import Tuple
 from modules.dataset_generator.configuration_loader import ConfigurationLoader
 from modules.dataset_generator.dataset_loader_creator import DatasetLoaderCreator
 from modules.dataset_generator.dataset_loader import DatasetLoader
 from modules.dataset_generator.dataset_strategy_creator import DatasetStrategyCreator
 from modules.dataset_generator.interfaces.strategy_interface import IDatasetGeneratorStrategy
 from modules.dataset_generator.interfaces.factory_interface import IFactory
+from modules.data_structures.dataset_config import DatasetConfig
+from modules.data_structures.processed_dataset import ProcessedDataset
+
 
 
 class DatasetGeneration:
@@ -15,18 +16,18 @@ class DatasetGeneration:
 
     def __init__(self, config_path: str, data_io_factory: IFactory, feature_processor_factory: IFactory, join_factory: IFactory, strategy_factory: IFactory):
         # Step 1: Load configuration using ConfigurationLoader 
-        config_loader: ConfigurationLoader = ConfigurationLoader(config_path)
-        self.config = config_loader.load_config()
+        config_loader = ConfigurationLoader(config_path)
+        self.config: DatasetConfig = config_loader.load_config()
 
         # Step 2: Instantiate DatasetLoader through the DatasetLoaderCreator to load data sources
-        dataset_loader_creator: DatasetLoaderCreator = DatasetLoaderCreator(data_io_factory)
-        self.dataset_loader: DatasetLoader = dataset_loader_creator.create_loader(self.config['sources'])
+        dataset_loader_creator = DatasetLoaderCreator(self.config, data_io_factory)
+        self.dataset_loader: DatasetLoader = dataset_loader_creator.create_loader()
 
         # Step 3: Use StrategyFactory to create dataset generation strategy 
-        dataset_strategy_creator: DatasetStrategyCreator = DatasetStrategyCreator(self.config['strategy'], self.config.get('join_operation', None), self.config['feature_processing'], feature_processor_factory, join_factory, strategy_factory)
+        dataset_strategy_creator = DatasetStrategyCreator(self.config, feature_processor_factory, join_factory, strategy_factory)
         self.dataset_strategy: IDatasetGeneratorStrategy = dataset_strategy_creator.create_strategy()
 
-    def generate(self) -> Tuple[pd.DataFrame, pd.DataFrame]: # TODO Consider adding interface for this datastructure or wrap it in something
+    def generate(self) -> ProcessedDataset:
         """
         Generate the dataset by loading data, optionally joining, and processing features.
         
@@ -38,6 +39,6 @@ class DatasetGeneration:
         dataframes = self.dataset_loader.load_data()
 
         # Generate the features and labels using the strategy
-        features_df, labels_df = self.dataset_strategy.generate(dataframes)
+        processed_dataset: ProcessedDataset = self.dataset_strategy.generate(dataframes)
 
-        return features_df, labels_df
+        return processed_dataset
--- modules/dataset_generator/dataset_loader_creator.py
+++ modules/dataset_generator/dataset_loader_creator.py
@@ -9,10 +9,11 @@ class DatasetLoaderCreator:
     Creates a DatasetLoader object with its arguments.
     It also calls the DataIOFactory to obtain the appropriate data readers for each source.
     """
-    def __init__(self, factory: IFactory):
+    def __init__(self, sources_config: list, factory: IFactory):
         self.factory = factory
+        self.sources_config = sources_config
 
-    def create_loader(self, sources_config: list) -> DatasetLoader:
+    def create_loader(self) -> DatasetLoader:
         """
         Creates a DatasetLoader object with its arguments based on the provided sources configuration.
         This involves calling the DataIOFactory to get the appropriate readers for each data source.
@@ -24,11 +25,11 @@ class DatasetLoaderCreator:
             DatasetLoader: A DatasetLoader instance with the appropriate data readers.
         """
         data_loaders = []
-        for source in sources_config:
+        for source in self.sources_config:
             file_type = source['file_type']
             data_io: DataIO = self.factory.create(file_type)
             data_loaders.append(data_io)
 
         # Return the object that implements the IDatasetLoader interface
-        return DatasetLoader(data_loaders, sources_config)
+        return DatasetLoader(data_loaders, self.sources_config)
 
