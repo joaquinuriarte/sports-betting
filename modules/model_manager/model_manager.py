58b1e2f65c8daae1240eb42f77d06c6f520e729d
Joaquin Uriarte <juriartebruno@gmail.com>
2024-10-24 12:04:54 -0700

    heheh

---
+++ modules/model_manager/model_manager.py
@@ -0,0 +1,14 @@
+
+########### Need to decide  ###############
+# 1. What is this?
+class ModelDataset:
+    examples: List[Example]
+
+    def __init__(self, examples: List[Example]):
+        self.examples = examples
+# 2. What is the Model datastructure we have in datastructures folder 
+
+########### Need to decide  ###############
+
+# We need a configuration loader
+# It needs to create the Model Config
