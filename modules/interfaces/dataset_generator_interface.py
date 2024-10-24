5deeb9abce90bf3f6b3060b26e514f1252fb412c
Joaquin Uriarte <juriartebruno@gmail.com>
2024-10-23 21:18:50 -0700

    Update dataset_generator_interface.py

---
+++ modules/interfaces/dataset_generator_interface.py
@@ -0,0 +1,20 @@
+# interfaces/idataset_generator.py
+from abc import ABC, abstractmethod
+from ..data_structures.processed_dataset import ProcessedDataset
+from typing import List
+import pandas as pd
+
+
+class IDatasetGenerator(ABC):
+    """
+    Interface for dataset generators.
+    """
+    @abstractmethod
+    def generate(self) -> "ProcessedDataset":
+        """
+        Generates the dataset, including features and labels.
+
+        Returns:
+            ProcessedDataset: An object containing the features and labels data.
+        """
+        pass
