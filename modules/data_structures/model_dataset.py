b84aa6360eab71fa489cdd4a11777ad5c1d9192e
Joaquin Uriarte <juriartebruno@gmail.com>
2024-10-25 16:12:46 -0700

    Created a ModelDataset datastructure

---
+++ modules/data_structures/model_dataset.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import List, Dict, Any
+
+# Represents a unique name for a model attribute.
+AttributeName = str
+
+# Describes an individual attribute, which can be either a feature or a label.
+# Attribute is a mapping from an attribute name to its data. Data can be a tensor or equivalent, depending on the model being used.
+Attribute = Dict[AttributeName, Any]
+
+@dataclass
+class Example:
+    """
+    Represents an individual data example containing features and labels.
+
+    Attributes:
+        features (List[Attribute]): The features associated with the example.
+        label (Attribute): The label or target value associated with the example.
+    """
+    features: List[Attribute]
+    label: Attribute
+
+@dataclass
+class ModelDataset:
+    """
+    Represents a dataset consisting of multiple examples for training.
+
+    Attributes:
+        examples (List[Example]): A list of data examples that contain features and labels.
+    """
+    examples: List[Example]
