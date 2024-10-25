ca2e45c3886330c17c2353da974669e0eee2f278
Joaquin Uriarte <juriartebruno@gmail.com>
2024-10-25 15:24:52 -0700

    Updated YAML and config model object

--- modules/data_structures/model_config.py
+++ modules/data_structures/model_config.py
@@ -1,9 +1,14 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Dict
+
 
 @dataclass
 class ModelConfig:
     """Contains model configurations."""
-
+    
+    training_epochs: int
+    learning_rate: float
+    optimizer: str
+    loss_function: str 
     model_path: Optional[str] = None
-    inference_mode: bool = True
+    architecture_config: Optional[Dict] = None
