005355861868d03feffb6988565a9e69561b1690
Joaquin Uriarte <juriartebruno@gmail.com>
2024-11-01 17:07:51 -0700

    Update model_interface.py

--- modules/model_manager/interfaces/model_interface.py
+++ modules/model_manager/interfaces/model_interface.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List, Any, Dict
+import pandas as pd
 from ...data_structures.model_dataset import ModelDataset, Attribute
 
 class IModel(ABC):
@@ -38,7 +39,7 @@ class IModel(ABC):
         pass
 
     @abstractmethod
-    def train(self, model_dataset: ModelDataset, epochs: int, batch_size: int):
+    def train(self, features: List[List[Attribute]], labels: List[List[Attribute]], epochs: int, batch_size: int):
         """
         Trains the model using the provided features and labels.
         Args:
@@ -50,7 +51,7 @@ class IModel(ABC):
         pass
 
     @abstractmethod
-    def predict(self, x: List[List[Attribute]]) -> List[Attribute]:
+    def predict(self, x: List[List[Attribute]]) -> pd.DataFrame:
         """
         Generates predictions for the provided input data.
         Args:
