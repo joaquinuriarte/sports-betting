b294048ed10a718370c24f6292913749933bf7b3
rmeizoso <meizosoricardo@gmail.com>
2024-11-09 01:46:35 -0800

    Add model dataset generator for getting Examples from dataframe.

---
+++ modules/dataset_generator/model_dataset_test.py
@@ -0,0 +1,26 @@
+from modules.dataset_generator.model_dataset import Example, InMemoryModelDataset
+from unittest import TestCase
+
+import pandas as pd # type: ingore
+
+
+class ModelDatasetTest(TestCase):
+
+    def test_df_conversion_to_model_dataset(self):
+        dataset = InMemoryModelDataset([])
+        df = pd.DataFrame(
+            {
+                "ints": [1],
+                "floats": [[2.0, 3.0]],
+                "strs": ["strs"],
+            }
+        )
+
+        dataset.load_from_dataframe(df, ["ints", "floats", "strs"])
+
+        expected_example = Example(
+            {"ints": [1], "floats": [2.0, 3.0], "strs": ["strs"]}
+        )
+        self.assertEqual(
+            InMemoryModelDataset([expected_example]).examples, dataset.examples
+        )
