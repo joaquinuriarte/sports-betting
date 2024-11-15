d3c20c63fddd4d0c9a70489c7aa217b7d10f37c2
Joaquin Uriarte <juriartebruno@gmail.com>
2024-11-15 08:21:32 -0800

    Refractored Dataset Generator
    Moved all of the factory functionality to the binary. Simplified this class heavily allowing easier testing

--- modules/dataset_generator/dataset_generator.py
+++ modules/dataset_generator/dataset_generator.py
@@ -1,17 +1,5 @@
-from modules.dataset_generator.configuration_loader import ConfigurationLoader
-from modules.dataset_generator.dataset_loader_creator import DatasetLoaderCreator
-from modules.dataset_generator.dataset_loader import DatasetLoader
-from modules.dataset_generator.dataset_strategy_creator import DatasetStrategyCreator
-from modules.dataset_generator.interfaces.strategy_interface import (
-    IDatasetGeneratorStrategy,
-)
-from ..interfaces.factory_interface import IFactory
-from modules.dataset_generator.interfaces.join_operator_interface import IJoinOperator
-from modules.dataset_generator.interfaces.feature_processor_operator_interface import (
-    IFeatureProcessorOperator,
-)
-from modules.dataset_generator.interfaces.data_io_interface import DataIO
-from modules.data_structures.dataset_config import DatasetConfig
+from .interfaces.dataset_loader_interface import IDatasetLoader
+from .interfaces.strategy_interface import IDatasetGeneratorStrategy
 from modules.data_structures.processed_dataset import ProcessedDataset
 from ..interfaces.dataset_generator_interface import IDatasetGenerator
 
@@ -23,27 +11,11 @@ class DatasetGenerator(IDatasetGenerator):
 
     def __init__(
         self,
-        config_path: str,
-        data_io_factory: IFactory[DataIO],
-        feature_processor_factory: IFactory[IFeatureProcessorOperator],
-        join_factory: IFactory[IJoinOperator],
-        strategy_factory: IFactory[IDatasetGeneratorStrategy],
+        dataset_loader: IDatasetLoader, 
+        dataset_strategy: IDatasetGeneratorStrategy, 
     ):
-        # Step 1: Load configuration using ConfigurationLoader
-        config_loader = ConfigurationLoader(config_path)
-        self.config: DatasetConfig = config_loader.load_config()
-
-        # Step 2: Instantiate DatasetLoader through the DatasetLoaderCreator to load data sources
-        dataset_loader_creator = DatasetLoaderCreator(self.config, data_io_factory)
-        self.dataset_loader: DatasetLoader = dataset_loader_creator.create_loader()
-
-        #  Step 3: Use StrategyFactory to create dataset generation strategy
-        dataset_strategy_creator = DatasetStrategyCreator(
-            self.config, feature_processor_factory, join_factory, strategy_factory
-        )
-        self.dataset_strategy: IDatasetGeneratorStrategy = (
-            dataset_strategy_creator.create_strategy()
-        )
+        self.dataset_loader = dataset_loader
+        self.dataset_strategy = dataset_strategy
 
     def generate(self) -> ProcessedDataset:
         """
