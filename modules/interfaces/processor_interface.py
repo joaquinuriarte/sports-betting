e70817e9fa6c28cd046b80027719873994a6927c
Joaquin Uriarte <juriartebruno@gmail.com>
2024-12-07 13:11:59 -0800

    Processor Interface

---
+++ modules/interfaces/processor_interface.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import Tuple, Optional
+from modules.data_structures.model_dataset import ModelDataset
+
+
+class IProcessor(ABC):
+    """
+    Interface for processors responsible for generating model datasets.
+    """
+
+    @abstractmethod
+    def generate(self, val_dataset_flag: Optional[bool] = True) -> Tuple[ModelDataset, Optional[ModelDataset]]:
+        """
+        Generates the training and optional validation datasets.
+
+        Args:
+            val_dataset_flag (Optional[bool]): Flag to indicate whether to create a validation dataset.
+
+        Returns:
+            Tuple[ModelDataset, Optional[ModelDataset]]: Training dataset and optionally a validation dataset.
+        """
+        pass
--- modules/processor/processor.py
+++ modules/processor/processor.py
@@ -4,11 +4,12 @@ from modules.data_structures.processed_dataset import ProcessedDataset
 from modules.interfaces.factory_interface import IFactory
 from modules.processor.interfaces.split_strategy_interface import ISplitStrategy
 from modules.processor.helpers.configuration_loader import ConfigurationLoader
+from modules.interfaces.processor_interface import IProcessor
 from modules.data_structures.model_dataset import ModelDataset, Example
 from typing import Tuple, Optional, List, Any
 
 
-class Processor(): #TODO Create and add interface for processor
+class Processor(IProcessor):
     """
     Main orchestrator for ModelDataset generation and train/test split.
     """
