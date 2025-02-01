8634d77b646373cdc3fee5f1db0c2497d89622be
Joaquin Uriarte <juriartebruno@gmail.com>
2025-01-31 16:27:11 -0800

    Update custom_loss_fxns.py

--- modules/model_manager/implementations/model_utils/custom_loss_fxns.py
+++ modules/model_manager/implementations/model_utils/custom_loss_fxns.py
@@ -16,3 +16,19 @@ def mse_plus_hinge_margin_loss(alpha=1.0):
 
         return mse_value + alpha * margin_loss
     return loss_fn
+
+
+def margin_sensitive_hinge_loss(delta=5.0):
+    def loss_fn(y_true, y_pred):
+        # 1) Mean Squared Error
+        mse_value = tf.reduce_mean(tf.square(y_true - y_pred))
+
+        # 2) The margins
+        actual_margin = y_true[:, 0] - y_true[:, 1]
+        pred_margin = y_pred[:, 0] - y_pred[:, 1]
+
+        product = actual_margin * pred_margin
+        # margin-sensitive hinge
+        losses = tf.nn.relu(delta - product)
+        return mse_value + tf.reduce_mean(losses)
+    return loss_fn
